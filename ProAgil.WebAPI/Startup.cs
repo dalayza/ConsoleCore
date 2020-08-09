6887468b10dd77d0797c506833e8e7c4cbce2bbd
Denis Alayza <denis.alayza@gmail.com>
2020-08-09 14:02:32 -0300

    Add project Angular SPA

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -28,7 +28,8 @@ namespace ProAgil.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
           services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
+          services.AddControllers();
+          services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +50,8 @@ namespace ProAgil.WebAPI
             {
                 endpoints.MapControllers();
             });
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
         }
     }
 }
